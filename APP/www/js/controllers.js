4ba457189cc617c87f3ffc11df0d9ad5e6139220
lucas <ukokuja@gmail.com>
2015-04-24 16:08:50 -0300

    personalizo candidatos

--- APP/www/js/controllers.js
+++ APP/www/js/controllers.js
@@ -70,8 +70,54 @@ angular.module('starter.controllers', [])
         })
 })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
+.controller('ChatDetailCtrl', function($scope, $stateParams) {
+        $scope.partidos = partidos;
+        $scope.candidatos = candidatos;
+        $scope.propuestas = propuestas;
+        $scope.temas = temitas;
+        $scope.selected = {
+            show: false
+        }
+        $scope.select = function(code){
+            if(!$scope.selected.show){
+                $scope.selected = $scope.temas[code];
+                $scope.selected.show = true;
+                $scope.selected.code = code;
+            }else{
+                if(code == $scope.selected.code){
+                    $scope.selected.show = false;
+                    $scope.selected.code = -10;
+                }else{
+                    $scope.selected = $scope.temas[code];
+                    $scope.selected.show = true;
+                    $scope.selected.code = code;
+                }
+            }
+        }
+        $scope.count = function(val){
+            return val;
+        }
+        console.log(candidatos);
+
+        angular.forEach($scope.candidatos, function(candidato){
+            if(!$scope.partidos[candidato.partido].candidatos)
+                $scope.partidos[candidato.partido].candidatos = [];
+            $scope.partidos[candidato.partido].candidatos.push(candidato);
+
+        })
+
+        angular.forEach($scope.propuestas, function(propuesta){
+            if(propuesta.candidato == $stateParams.candidato){
+                if(!$scope.candidatos[propuesta.candidato].propuestas)
+                    $scope.candidatos[propuesta.candidato].propuestas = []
+                $scope.candidatos[propuesta.candidato].propuestas.push(propuesta);
+                if(!$scope.temas[propuesta.tema.codigo].propuestas)
+                    $scope.temas[propuesta.tema.codigo].propuestas = [];
+                $scope.temas[propuesta.tema.codigo].propuestas.push(propuesta);
+            }
+            $scope.candidato = $scope.candidatos[$stateParams.candidato];
+        })
+
 })
 
 .controller('AccountCtrl', function($scope) {
